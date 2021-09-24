e30f89126b30d9ff9e8a4b4af6527a86d1926433
djememahamat <djeme@mahamat.email>
2021-09-24 17:32:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ const port = 3000;
 //Loads the handlebars module
 const handlebars = require('express-handlebars');
 //Sets our app to use the handlebars engine
-app.set('view engine', 'handlebars');
-//Sets handlebars configurations (we will go through them later on)
-app.engine('handlebars', handlebars({
+
+app.set('view engine', 'hbs');
+//Sets handlebars configurations
+app.engine('hbs', handlebars({
 layoutsDir: __dirname + '/views/layouts',
+//new configuration parameter
+extname: 'hbs'
 }));
+
 app.use(express.static('public'))
 app.get('/', (req, res) => {
 //Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
