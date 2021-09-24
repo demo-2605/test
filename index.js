e72f935b679573d3af779e7703ec2b65dbaadc3a
djememahamat <djeme@mahamat.email>
2021-09-24 17:44:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -16,9 +16,32 @@ partialsDir: __dirname + '/views/partials/'
 app.use(express.static('public'))
 
 //I would like to use a real api but let's use this for the sake of //the simplicity of the article
-fakeApi = () => 'Faker';
+fakeApi = () => {
+  return [
+    {
+      name: 'Katarina',
+      lane: 'midlaner'
+    },
+    {
+      name: 'Jayce',
+      lane: 'toplaner'
+    },
+    {
+      name: 'Heimerdinger',
+      lane: 'toplaner'
+    },
+    {
+      name: 'Zed',
+      lane: 'midlaner'
+    },
+    {
+      name: 'Azir',
+      lane: 'midlaner'
+    }
+  ];
+}
 app.get('/', (req, res) => {
-res.render('main', {layout: 'index', proPlayer: fakeApi()});
+res.render('main', {layout: 'index', suggestedChamps: fakeApi(), listExists: true});
 });
 
 app.listen(port, () => console.log(`App listening to port ${port}`));
