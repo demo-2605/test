5e0a24760901fb9d5abd90bcb9354f6bdda25ce8
djememahamat <djeme@mahamat.email>
2021-09-24 17:41:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -14,9 +14,11 @@ partialsDir: __dirname + '/views/partials/'
 }));
 
 app.use(express.static('public'))
+
+//I would like to use a real api but let's use this for the sake of //the simplicity of the article
+fakeApi = () => 'Faker';
 app.get('/', (req, res) => {
-//Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
-res.render('main', {layout : 'index'});
+res.render('main', {layout: 'index', proPlayer: fakeApi()});
 });
 
 app.listen(port, () => console.log(`App listening to port ${port}`));
