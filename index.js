2f3a92c777d8c0378b38df552f8f54cdb1dc0b03
djememahamat <djeme@mahamat.email>
2021-09-24 19:23:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -43,8 +43,35 @@ fakeApi = () => {
 
 categories = ['jazz', 'soul', 'French variety']
 
+products = [
+    {
+      id: 'the-jazz-story'
+      image: 'https://3jsapw7i3u.eu-west-1.awsapprunner.com/uploads/thumbnail_the_jazz_story_eab962a04b.jpeg',
+      name: 'The Jazz Story',
+      description: "Édith Piaf was a French singer-songwriter, cabaret performer and film actress noted as France's national chanteuse and one of the country's most widely known international stars. Piaf's music was often autobiographical, and she specialized in chanson and torch ballads about love, loss and sorrow"
+    },
+    {
+      id: 'the-jazz-story'
+      image: 'https://3jsapw7i3u.eu-west-1.awsapprunner.com/uploads/thumbnail_the_jazz_story_eab962a04b.jpeg',
+      name: 'The Jazz Story 2',
+      description: "Édith Piaf was a French singer-songwriter, cabaret performer and film actress noted as France's national chanteuse and one of the country's most widely known international stars. Piaf's music was often autobiographical, and she specialized in chanson and torch ballads about love, loss and sorrow"
+    },
+    {
+      id: 'the-jazz-story'
+      image: 'https://3jsapw7i3u.eu-west-1.awsapprunner.com/uploads/thumbnail_the_jazz_story_eab962a04b.jpeg',
+      name: 'The Jazz Story 3',
+      description: "Édith Piaf was a French singer-songwriter, cabaret performer and film actress noted as France's national chanteuse and one of the country's most widely known international stars. Piaf's music was often autobiographical, and she specialized in chanson and torch ballads about love, loss and sorrow"
+    },
+    {
+      id: 'the-jazz-story'
+      image: 'https://3jsapw7i3u.eu-west-1.awsapprunner.com/uploads/thumbnail_the_jazz_story_eab962a04b.jpeg',
+      name: 'The Jazz Story 4',
+      description: "Édith Piaf was a French singer-songwriter, cabaret performer and film actress noted as France's national chanteuse and one of the country's most widely known international stars. Piaf's music was often autobiographical, and she specialized in chanson and torch ballads about love, loss and sorrow"
+    }
+  ]
+
 app.get('/', (req, res) => {
-res.render('main', {layout: 'index', suggestedChamps: fakeApi(), listExists: true, categories: categories});
+res.render('main', {layout: 'index', suggestedChamps: fakeApi(), listExists: true, categories: categories, products: products});
 });
 
 app.listen(port, () => console.log(`App listening to port ${port}`));
